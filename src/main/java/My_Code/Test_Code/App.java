f67387007786be5d62d1ac8b8809f547ce7551a7
aashish.mohankar <aashish.mohankar@click2cloud.net>
2019-04-04 17:52:29 +0530

    Initial commit

---
+++ src/main/java/My_Code/Test_Code/App.java
@@ -0,0 +1,13 @@
+package My_Code.Test_Code;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/Test_simple_code/Hello_World.java
@@ -0,0 +1,10 @@
+package Test_simple_code;
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		
+      System.out.println("Hello World");
+	}
+
+}
