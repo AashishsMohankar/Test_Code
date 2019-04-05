0cd701300b1e08a9c369f2c25a7acdeb387e94f7
aashish.mohankar <aashish.mohankar@click2cloud.net>
2019-04-05 16:16:57 +0530

    add addition program

--- src/main/java/My_Code/Test_Code/App.java
+++ src/main/java/My_Code/Test_Code/App.java
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
+		int a=5;
+		int b=2;
+		sum=0;
+		
+		sum= a+b;
+		System.out.println( "Addition of a and b is", +sum );
         System.out.println( "Hello World!" );
     }
 }
--- src/main/java/Test_simple_code/Hello_World.java
+++ src/main/java/Test_simple_code/Hello_World.java
@@ -3,8 +3,14 @@ package Test_simple_code;
 public class Hello_World {
 
 	public static void main(String[] args) {
+	{
+		int a=5;
+		int b=2;
+		sum=0;
 		
-      System.out.println("Hello World");
+		sum= a+b;
+		System.out.println( "Addition of a and b is", +sum );
+        System.out.println( "Hello World!" );
 	}
 
 }
