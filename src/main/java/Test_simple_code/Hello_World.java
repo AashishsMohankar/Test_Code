b6e9e2c4f6e561a2cbfc63c4be8323d27fdc4f24
aashish.mohankar <aashish.mohankar@click2cloud.net>
2019-04-09 10:42:12 +0530

    Remove program changes

--- src/main/java/Test_simple_code/Hello_World.java
+++ src/main/java/Test_simple_code/Hello_World.java
@@ -4,12 +4,6 @@ public class Hello_World {
 
 	public static void main(String[] args) {
 	{
-		/*int a=5;
-		int b=2;
-		sum=0;
-		
-		sum= a+b;
-		System.out.println( "Addition of a and b is", +sum );*/
         System.out.println( "Hello World!" );
 	}
 
